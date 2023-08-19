f11882d039b7c370009034c872139caa9948ee4e
Zefir Kirilov <contact@zefir.pro>
2023-08-19 12:48:56 +0300

    add config with per-enchant options for max level and cost

--- src/main/java/pro/cloudnode/smp/enchantbookplus/EnchantBookPlus.java
+++ src/main/java/pro/cloudnode/smp/enchantbookplus/EnchantBookPlus.java
@@ -1,9 +1,22 @@
 package pro.cloudnode.smp.enchantbookplus;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 import pro.cloudnode.smp.enchantbookplus.event.PrepareAnvil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+
 public final class EnchantBookPlus extends JavaPlugin {
+    /**
+     * Get plugin instance.
+     */
+    public static EnchantBookPlus getInstance() {
+        return getPlugin(EnchantBookPlus.class);
+    }
+
     /**
      * Register event listeners.
      */
@@ -11,9 +24,47 @@ public final class EnchantBookPlus extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PrepareAnvil(), this);
     }
 
+    /**
+     * Config enchantments cache
+     */
+    private @NotNull List<@NotNull ConfigEnchantmentEntry> configEnchantments = new ArrayList<>();
+
+    /**
+     * Config enchantments cache
+     */
+    public @NotNull List<@NotNull ConfigEnchantmentEntry> getConfigEnchantments() {
+        if (configEnchantments.size() == 0) reload();
+        return configEnchantments;
+    }
+
+    /**
+     * Get enchantment from cache
+     *
+     * @param name Name of the enchantment
+     */
+    public @NotNull Optional<@NotNull ConfigEnchantmentEntry> getConfigEnchantment(final @NotNull String name) {
+        return getConfigEnchantments().stream().filter(enchantment -> enchantment.getName().equalsIgnoreCase(name) || enchantment.getName().equalsIgnoreCase("ALL")).findFirst();
+    }
+
+    /**
+     * Reload config
+     */
+    public void reload() {
+        reloadConfig();
+        try {
+            configEnchantments = ConfigEnchantmentEntry.config(getConfig().get("enchantments"));
+        }
+        catch (final @NotNull Exception exception) {
+            getLogger().log(Level.SEVERE, "Failed to load config", exception);
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
     @Override
     public void onEnable() {
         registerEvents();
+        saveDefaultConfig();
+        reload();
     }
 
     @Override
--- src/main/java/pro/cloudnode/smp/enchantbookplus/event/PrepareAnvil.java
+++ src/main/java/pro/cloudnode/smp/enchantbookplus/event/PrepareAnvil.java
@@ -9,10 +9,15 @@ import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.jetbrains.annotations.NotNull;
+import pro.cloudnode.smp.enchantbookplus.ConfigEnchantmentEntry;
+import pro.cloudnode.smp.enchantbookplus.EnchantBookPlus;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
-public class PrepareAnvil implements Listener {
+public final class PrepareAnvil implements Listener {
     @EventHandler
     public void onPrepareAnvil(final @NotNull PrepareAnvilEvent event) {
         final @NotNull Optional<@NotNull ItemStack> result = Optional.ofNullable(event.getResult());
@@ -26,27 +31,41 @@ public class PrepareAnvil implements Listener {
         final @NotNull Map<@NotNull Enchantment, @NotNull Integer> upgradeEnchants =
                 upgrade.getType() == Material.ENCHANTED_BOOK && upgrade.getItemMeta() instanceof final @NotNull EnchantmentStorageMeta upgradeMeta ?
                         upgradeMeta.getStoredEnchants() : upgrade.getEnchantments();
-        if (itemEnchants.isEmpty() || upgradeEnchants.isEmpty()) return;
+        if (upgradeEnchants.isEmpty()) return;
         final @NotNull HashMap<@NotNull Enchantment, @NotNull Integer> upgrades = new HashMap<>();
+        int cost = 0;
         for (final @NotNull Map.Entry<@NotNull Enchantment, @NotNull Integer> entry : upgradeEnchants.entrySet()) {
             final @NotNull Enchantment enchantment = entry.getKey();
+            if (enchantment.getMaxLevel() == 1) continue;
+            final @NotNull Optional<@NotNull ConfigEnchantmentEntry> configEnchantment = EnchantBookPlus.getInstance().getConfigEnchantment(enchantment.getKey().getKey());
+            if (configEnchantment.isEmpty()) continue;
             final int upgradeLevel = entry.getValue();
-            if (upgradeLevel <= enchantment.getMaxLevel()) continue;
+            final int finalLevel;
             if (itemEnchants.containsKey(enchantment)) {
                 final int itemLevel = itemEnchants.get(enchantment);
-                if (itemLevel > upgradeLevel) upgrades.put(enchantment, itemLevel);
-                else if (itemLevel == upgradeLevel) upgrades.put(enchantment, upgradeLevel + 1);
-                else upgrades.put(enchantment, upgradeLevel);
+                if (itemLevel > upgradeLevel) finalLevel = itemLevel;
+                else if (itemLevel == upgradeLevel) finalLevel = upgradeLevel + 1;
+                else finalLevel = upgradeLevel;
             }
-            else upgrades.put(enchantment, upgradeLevel);
+            else finalLevel = upgradeLevel;
+            if (finalLevel < enchantment.getMaxLevel()) continue;
+            if (configEnchantment.get().getMaxLevel(enchantment).isPresent() && finalLevel > configEnchantment.get().getMaxLevel(enchantment).get()) continue;
+            if (finalLevel > upgradeLevel) cost += configEnchantment.get().getMultiplyCostByLevel() ? configEnchantment.get().getCost() * (finalLevel - enchantment.getMaxLevel()) : configEnchantment.get().getCost();
+            upgrades.put(enchantment, finalLevel);
         }
         if (upgrades.isEmpty()) return;
+        final int vanillaCost = inventory.getRepairCost();
+        inventory.setRepairCost(vanillaCost + cost);
         for (final @NotNull Map.Entry<@NotNull Enchantment, @NotNull Integer> entry : upgrades.entrySet()) {
             if (result.get().getItemMeta() instanceof final @NotNull EnchantmentStorageMeta resultMeta) {
+                if (!resultMeta.getStoredEnchants().containsKey(entry.getKey())) continue;
                 resultMeta.addStoredEnchant(entry.getKey(), entry.getValue(), true);
                 result.get().setItemMeta(resultMeta);
             }
-            else result.get().addUnsafeEnchantment(entry.getKey(), entry.getValue());
+            else {
+                if (!result.get().getEnchantments().containsKey(entry.getKey())) continue;
+                result.get().addUnsafeEnchantment(entry.getKey(), entry.getValue());
+            }
         }
     }
 }

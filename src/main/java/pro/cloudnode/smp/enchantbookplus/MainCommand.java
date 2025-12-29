84bb71f11ef6f2c659f110a659e24c624f7fbcdf
Zefir Kirilov <contact@zefir.pro>
2025-12-29 13:46:54 +0200

    add support for Spigot and older versions (1.17–1.21.11)

--- src/main/java/pro/cloudnode/smp/enchantbookplus/MainCommand.java
+++ src/main/java/pro/cloudnode/smp/enchantbookplus/MainCommand.java
@@ -1,15 +1,13 @@
 package pro.cloudnode.smp.enchantbookplus;
 
-import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Optional;
 
 public final class MainCommand implements CommandExecutor, TabCompleter {
     private final @NotNull EnchantBookPlus plugin;
@@ -19,7 +17,12 @@ public final class MainCommand implements CommandExecutor, TabCompleter {
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+    public boolean onCommand(
+            final @NotNull CommandSender sender,
+            final @NotNull Command command,
+            final @NotNull String label,
+            final @NotNull String @NotNull [] args
+    ) {
         if (args.length == 1 && args[0].equalsIgnoreCase("reload"))
             return reload(sender, command);
 
@@ -27,7 +30,12 @@ public final class MainCommand implements CommandExecutor, TabCompleter {
     }
 
     @Override
-    public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+    public @NotNull List<String> onTabComplete(
+            final @NotNull CommandSender sender,
+            final @NotNull Command command,
+            final @NotNull String label,
+            final @NotNull String @NotNull [] args
+    ) {
         if (sender.hasPermission(Permissions.RELOAD))
             return List.of("reload");
 
@@ -37,15 +45,11 @@ public final class MainCommand implements CommandExecutor, TabCompleter {
     /**
      * Plugin overview
      */
-    @SuppressWarnings({"UnstableApiUsage", "SameReturnValue"})
+    @SuppressWarnings("SameReturnValue")
     public boolean overview(final @NotNull CommandSender sender) {
-        sender.sendMessage(MiniMessage.miniMessage()
-                .deserialize(
-                        "<green><name></green> <white>v<version> by</white> <gray><author></gray>",
-                        Placeholder.unparsed("name", plugin.getPluginMeta().getName()),
-                        Placeholder.unparsed("version", plugin.getPluginMeta().getVersion()),
-                        Placeholder.unparsed("author", String.join(", ", plugin.getPluginMeta().getAuthors()))
-                ));
+        PluginDescriptionFile description = plugin.getDescription();
+        sender.sendMessage("§а" + description.getName() + " §fv" + description.getVersion() + " by §7"
+                + String.join(", ", description.getAuthors()));
 
         return true;
     }
@@ -55,15 +59,12 @@ public final class MainCommand implements CommandExecutor, TabCompleter {
      */
     @SuppressWarnings("SameReturnValue")
     public boolean reload(final @NotNull CommandSender sender, final @NotNull Command command) {
-        if (!sender.hasPermission(Permissions.RELOAD)) {
-            sender.sendMessage(Optional.ofNullable(command.permissionMessage())
-                            .orElse(sender.getServer().permissionMessage()));
-            return true;
-        }
+        if (!sender.hasPermission(Permissions.RELOAD))
+            return overview(sender);
 
         plugin.reload();
 
-        sender.sendMessage(MiniMessage.miniMessage().deserialize("<green>(!) Plugin configuration reloaded."));
+        sender.sendMessage("§a(!) Plugin configuration reloaded.");
 
         return true;
     }
